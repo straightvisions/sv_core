bc12ee140b53b4044b6e89c8fe137fa7592c5d3c
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2018-10-17 15:50:52 +0200

    FEAT - REFACTORING - SETTINGS

--- backend/tpl/section_settings.php
+++ backend/tpl/section_settings.php
@@ -2,10 +2,7 @@
 <section id="section_<?php echo $section_name; ?>" class="sv_admin_section">
 	<h1 class="section_title"><?php echo ucfirst($section['type']) ;?></h1>
 	<div class="section_content">
-		<h3 class="divider">Settings</h3>
-		<?php
-			echo static::$settings->get_module_settings_form($section['object']);
-		?>
+	<?php echo static::$settings->get_module_settings_form($section['object']); ?>
 	</div>
 </section>
 <?php }
