817c77a90d43420d45158f720e65ceb96b791a9a
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2018-10-17 11:06:35 +0200

    PATCH - STYLE - ABOUT TPL

--- assets/admin.js
+++ assets/admin.js
@@ -1,8 +1,8 @@
-jQuery('.sv_side_menu_item').click(function() {
+jQuery('.sv_admin_menu_item').click(function() {
 	if(!jQuery(this).hasClass('active')) {
-		jQuery('.sv_side_menu_item.active').removeClass('active');
+		jQuery('.sv_admin_menu_item.active').removeClass('active');
 		jQuery(this).addClass('active');
-		jQuery('.sv_content_wrapper').css('display', 'none');
+		jQuery('.sv_admin_section').css('display', 'none');
 		jQuery(jQuery(this).attr('href')).css('display', 'flex');
 	}
 });
