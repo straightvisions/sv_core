9c52f6b8e0ed331b4bed3f7c4c70d49f3aeabc6c
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2018-10-17 20:11:15 +0200

    FEAT - REFACTORING - SECTIONS

--- backend/tpl/about.php
+++ backend/tpl/about.php
@@ -2,12 +2,33 @@
 <section id="section_about" class="sv_admin_section">
 	<h1 class="section_title">About</h1>
 	<div class="section_content">
-		<h3 class="divider">Info</h3>
-		<ul class="info_list">
-			<li>Name: <span><?php echo get_admin_page_title(); ?></span></li>
-			<li>Version: <span>1.0.3</span></li>
-			<li>Status: <span>active</span></li>
-		</ul>
+		<div class="col-50">
+			<h3 class="divider">Info</h3>
+			<ul class="info_list">
+				<li>Name: <span><?php echo get_admin_page_title(); ?></span></li>
+				<li>Version: <span>1.0.3</span></li>
+				<li>Status: <span>active</span></li>
+			</ul>
+		</div>
+		<div class="col-50">
+			<h3 class="divider">Description</h3>
+			<p class="instance_description">
+				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+			</p>
+		</div>
+		<div>
+			<h3 class="divider">Active Modules</h3>
+			<ul class="instance_list">
+				<li>[ Module Name ]</li>
+				<li>[ Module Name ]</li>
+				<li>[ Module Name ]</li>
+				<li>[ Module Name ]</li>
+				<li>[ Module Name ]</li>
+				<li>[ Module Name ]</li>
+				<li>[ Module Name ]</li>
+				<li>[ Module Name ]</li>
+			</ul>
+		</div>
 	</div>
 </section>
 <?php }
--- backend/tpl/section_instance.php
+++ backend/tpl/section_instance.php
@@ -64,18 +64,20 @@
 			<p><?php _e('Enqueue SV style for admin menu pages:', $section['object']->get_module_name()); ?></p>
 			<code>$this->acp_style($hook=false);</code>
 		</div>
-		<h3 class="divider"><?php _e('Path Structure', $section['object']->get_module_name()); ?></h3>
-		<p><?php _e('To encourage following default path structures and to make maintenance easier, the following methods follow the default structure.', $section['object']->get_module_name()); ?></p>
-		<code>$this->get_path_lib($suffix='',$check_if_exists=false);</code>
-		<blockquote><?php echo $section['object']->get_path_lib(); ?></blockquote>
-		<code>$this->get_url_lib($suffix='',$check_if_exists=false);</code>
-		<blockquote><?php echo $section['object']->get_url_lib(); ?></blockquote>
-		<code>$this->get_path_lib_modules();</code>
-		<blockquote><?php echo $section['object']->get_path_lib_modules(); ?></blockquote>
-		<code>$this->get_path_lib_section($section=false,$dir=false,$suffix='',$check_if_exists=false);</code>
-		<blockquote><?php echo $section['object']->get_path_lib_section(); ?></blockquote>
-		<code>$this->get_url_lib_section($section=false,$dir=false,$suffix='',$check_if_exists=false);</code>
-		<blockquote><?php echo $section['object']->get_url_lib_section(); ?></blockquote>
+		<div>
+			<h3 class="divider"><?php _e('Path Structure', $section['object']->get_module_name()); ?></h3>
+			<p><?php _e('To encourage following default path structures and to make maintenance easier, the following methods follow the default structure.', $section['object']->get_module_name()); ?></p>
+			<code>$this->get_path_lib($suffix='',$check_if_exists=false);</code>
+			<blockquote><?php echo $section['object']->get_path_lib(); ?></blockquote>
+			<code>$this->get_url_lib($suffix='',$check_if_exists=false);</code>
+			<blockquote><?php echo $section['object']->get_url_lib(); ?></blockquote>
+			<code>$this->get_path_lib_modules();</code>
+			<blockquote><?php echo $section['object']->get_path_lib_modules(); ?></blockquote>
+			<code>$this->get_path_lib_section($section=false,$dir=false,$suffix='',$check_if_exists=false);</code>
+			<blockquote><?php echo $section['object']->get_path_lib_section(); ?></blockquote>
+			<code>$this->get_url_lib_section($section=false,$dir=false,$suffix='',$check_if_exists=false);</code>
+			<blockquote><?php echo $section['object']->get_url_lib_section(); ?></blockquote>
+		</div>
 	</div>
 </section>
 <?php }
