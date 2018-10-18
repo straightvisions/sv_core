51b079f6735f87e44bc7eedf6745c75221726f04
Matthias Reuter <matthias-reuter@straightvisions.com>
2018-10-18 13:40:07 +0200

    CORE - VERSION - INCREASE

--- abstract.php
+++ abstract.php
@@ -3,7 +3,7 @@
 	namespace sv_core;
 
 	abstract class sv_abstract{
-		const version_core					= 1003;
+		const version_core					= 1004;
 		
 		protected $name						= false;
 		protected $module_name				= false;
