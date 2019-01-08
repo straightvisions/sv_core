f89619d6a04f27374b2fba1937355a3238609d3b
Matthias Reuter <matthias-reuter@straightvisions.com>
2019-01-08 11:17:40 +0100

    BUG - FIX - MISSING NOTICES

--- core.php
+++ core.php
@@ -7,6 +7,7 @@ if ( !class_exists( '\sv_core\core' ) ) {
 	
 	class core extends sv_abstract {
 		public static $log                  = false;
+		public static $notices				= false;
 		public static $settings				= false;
 		public static $curl	    			= false;
 		public static $widgets				= false;
