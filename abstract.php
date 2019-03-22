adfc4aa00946c37f83e8bc05462f809854f7e088
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2019-03-22 14:40:53 +0100

    Increased version number

--- abstract.php
+++ abstract.php
@@ -3,7 +3,7 @@
 namespace sv_core;
 
 abstract class sv_abstract {
-	const version_core					= 3011;
+	const version_core					= 3012;
 
 	protected $name						= false;
 	protected $module_name				= false;
