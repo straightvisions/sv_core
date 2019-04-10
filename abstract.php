21f96c1b7deac554b4414f02caef9b07561c8a23
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-04-10 16:20:09 +0200

    Set core version number to the correct current one

--- abstract.php
+++ abstract.php
@@ -3,7 +3,7 @@
 namespace sv_core;
 
 abstract class sv_abstract {
-	const version_core					= 3015;
+	const version_core					= 3016;
 
 	protected $name						= false;
 	protected $module_name				= false;
