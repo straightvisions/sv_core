a77016f71fdb6459a293945dfcf1d05543983387
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-05-14 17:13:16 +0200

    Updated core version

--- abstract.php
+++ abstract.php
@@ -3,7 +3,7 @@
 namespace sv_core;
 
 abstract class sv_abstract {
-	const version_core					= 3103;
+	const version_core					= 3111;
 
 	protected $name						= false;
 	protected $module_name				= false;
