b19c32d9b65806e8529b97b49b8d7dfa03a4dc65
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-06-03 14:38:54 +0200

    UPDATE

--- php_version.php
+++ php_version.php
@@ -1,8 +1,11 @@
 <?php
-	function sv_core_php_version_check(){
-		if(version_compare( phpversion(), '7.0.0', '>=' )){
-			return true;
-		}else{
-			return false;
-		}
-	}
+	add_action( 'admin_notices', function() use ($name, $min_php){
+		?>
+		<div class="update-nag">
+			<?php echo 'You need to update your PHP version to run '. $name; ?> <br/>
+			<?php echo 'Actual version is:'; ?>
+			<strong><?php echo phpversion(); ?></strong>, <?php echo 'required is'; ?>
+			<strong><?php echo $min_php; ?></strong>
+		</div>
+		<?php
+	} );
