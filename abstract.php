0a8b9d769d5eb42e8ac6650c99744b2dc851663d
Matthias Reuter <info@straightvisions.com>
2019-06-08 17:52:31 +0200

    UPDATE VERSION

--- abstract.php
+++ abstract.php
@@ -3,7 +3,7 @@
 namespace sv_core;
 
 abstract class sv_abstract {
-	const version_core					= 3126;
+	const version_core					= 3127;
 
 	protected $name						= false;
 	protected $module_name				= false;
