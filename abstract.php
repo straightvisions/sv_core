1689b6f7d550920f131b11facf7cf607a4feff42
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-06-19 14:53:55 +0200

    SMALL ENHANCEMENTS

--- abstract.php
+++ abstract.php
@@ -3,7 +3,7 @@
 namespace sv_core;
 
 abstract class sv_abstract {
-	const version_core					= 3133;
+	const version_core					= 3134;
 
 	protected $name						= false;
 	protected $module_name				= false;
