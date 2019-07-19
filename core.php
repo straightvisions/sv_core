d19d1c5888a1a76546e0b6c1422a73e8e28abe68
Matthias Bathke <info@straightvisions.com>
2019-07-19 15:26:01 +0200

    FEAT - ADD - FREEMIUS

--- core.php
+++ core.php
@@ -43,6 +43,13 @@ if ( !class_exists( '\sv_core\core' ) ) {
 				self::$path_core			= trailingslashit( dirname( __FILE__ ) );
 				self::$url_core				= trailingslashit( get_site_url() ) . str_replace( ABSPATH,'', self::$path_core );
 
+				require_once( 'freemius/start.php' );
+
+				static::$settings = new settings;
+				static::$settings->set_root( $this->get_root() );
+				static::$settings->set_parent( $this );
+
+
 				require_once( 'settings/settings.php' );
 
 				static::$settings = new settings;
