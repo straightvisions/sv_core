721625004cc4899d9ee0df83b7d4976b046083f9
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-07-19 14:11:54 +0200

    Added mobile support for settings tabs

--- abstract.php
+++ abstract.php
@@ -2,7 +2,7 @@
 	namespace sv_core;
 	
 	abstract class sv_abstract {
-		const version_core					= 4012;
+		const version_core					= 4013;
 		
 		protected $name						= false;
 		protected $module_name				= false;
