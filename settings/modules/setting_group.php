a07fb13d0be634cc064e0050195d9db61f1a0efb
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2019-11-11 16:22:08 +0100

    Fixed complex bug and optimized id handling

--- settings/modules/setting_group.php
+++ settings/modules/setting_group.php
@@ -80,10 +80,10 @@
 				foreach($this->get_children() as $child) {
 					$fields[]			= '<div class="'.$this->get_prefix($this->get_type()).'_item">';
 					$fields[]			= '<div class="sv_'.$this->get_module_name().'_input" data-sv_input_name="'.$child->get_field_id().'[sv_form_field_index]['.$child->get_ID().']'.'">'.$child->run_type()->html(
-							($setting_id !== false ? $child->get_field_id().'['.$i.']['.$child->get_ID().']' : $child->get_field_id().'[sv_form_field_index]['.$child->get_ID().']'),
+							($setting_id !== false ? $child->get_field_id().'['.$setting_id.']['.$child->get_ID().']' : $child->get_field_id().'[sv_form_field_index]['.$child->get_ID().']'),
 							$child->get_title(),
 							$child->get_description(),
-							($setting_id !== false ? $child->get_field_id().'['.$i.']['.$child->get_ID().']' : ''),
+							($setting_id !== false ? $child->get_field_id().'['.$setting_id.']['.$child->get_ID().']' : ''),
 							(
 								(
 								$setting_id !== false &&
@@ -126,7 +126,7 @@
 
 				}
 
-				$header[]				= ($setting_id !== false ? '<div class="sv_'.$this->get_module_name().'">' : '');
+				$header[]				= ($setting_id !== false ? '<div class="sv_'.$this->get_module_name().'" sv_'.$this->get_module_name().'_entry_id="'.$setting_id.'">' : '');
 				$header[]				= '
 					<div class="sv_'.$this->get_module_name().'_header">
 						<h4 class="sv_' .$this->get_module_name() .'_title"><i class="fas fa-angle-right"></i> '.$label.'</h4> 
