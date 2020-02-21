fa1d22e3483ecae03d66fc78e3a1bd0bd3c58727
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-02-21 14:11:40 +0100

    BUG - FIX - SCRIPT ENQUEUE

--- abstract.php
+++ abstract.php
@@ -2,7 +2,7 @@
 	namespace sv_core;
 	
 	abstract class sv_abstract {
-		const version_core					= 4022;
+		const version_core					= 4023;
 		
 		protected $name						= false;
 		protected $module_name				= false;
