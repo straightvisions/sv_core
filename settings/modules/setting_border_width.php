dd368c2c153fdc9fd368579e8a1d979312e0b8ae
Adrian Chudzynski <adrian-chudzynski@straightvisions.com>
2020-02-26 13:50:57 +0100

    Added border_width settings type

---
+++ settings/modules/setting_border_width.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace sv_core;
+
+class setting_border_width extends settings{
+	private $parent				= false;
+
+	public function __construct($parent=false){
+		$this->parent			= $parent;
+	}
+	public function html($ID, $title, $description, $name, $value, $required, $disabled, $placeholder, $maxlength, $minlength) {
+		ob_start();
+		require($this->get_path_core('settings/tpl/'.basename(__FILE__)));
+		$setting = ob_get_contents();
+		ob_end_clean();
+		return $setting;
+	}
+	protected function print_sub_field($ID, $title, $description, $name, $value, $required, $disabled, $placeholder, $maxlength, $minlength, string $sub){
+		require($this->get_path_core('settings/tpl/setting_margin_field.php'));
+	}
+}
