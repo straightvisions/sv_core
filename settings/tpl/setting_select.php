e99c825025eff9eb2a6bb16ea0be7184357870f4
Matthias Bathke <info@straightvisions.com>
2020-03-04 11:21:19 +0100

    FIX SETTING

--- settings/tpl/setting_select.php
+++ settings/tpl/setting_select.php
@@ -8,7 +8,7 @@
 <?php echo $required; ?>
 <?php echo $disabled; ?>>
 <?php
-	foreach( $this->get_parent()->get_options() as $o_value => $o_name ) {
+	foreach( $this->get_options() as $o_value => $o_name ) {
 		echo '<option
 		' . ( ( $value == $o_value ) ? ' selected="selected"' : '' ) . '
 		value="' . $o_value . '">' . $o_name . '</option>';
