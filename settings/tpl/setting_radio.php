177c2549b97a724fdb979f09e86c83d10de8b60a
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-03-20 12:33:21 +0100

    FIX Settings

--- settings/tpl/setting_radio.php
+++ settings/tpl/setting_radio.php
@@ -5,7 +5,7 @@ echo ( $radio_style == 'switch'
 ? '<div class="sv_radio_switch_wrapper"><label for="' . $ID . '"><div class="switch_field">'
 			: '' );
 
-			foreach( $this->get_parent()->get_options() as $o_value => $o_name ) {
+			foreach( $this->get_options() as $o_value => $o_name ) {
 			echo ( $radio_style == 'switch' ? '' : '<label for="' . $ID . '">' );
 				echo
 				'<input
