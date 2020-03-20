aec67f10a253dd6e6ee24c5511d5e09e390b6766
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-03-20 14:33:12 +0100

    REMOVE RUN TYPE FROM WIDGETS

--- widgets/widgets.php
+++ widgets/widgets.php
@@ -134,7 +134,7 @@
 				}
 				
 				foreach ($this->get_class()->get_widget_settings() as $setting) {
-					echo $setting->run_type()->widget( ( isset ( $instance[ $setting->get_ID() ] ) ? $instance[ $setting->get_ID() ] : $setting->run_type()->get_default_value() ), $this );
+					echo $setting->widget( ( isset ( $instance[ $setting->get_ID() ] ) ? $instance[ $setting->get_ID() ] : $setting->get_default_value() ), $this );
 				}
 			}
 		}
