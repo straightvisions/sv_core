380cc692d1099f9f329a6b220522ab9235183bd8
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-05-05 13:16:40 +0200

    UPDATE VERSION

--- abstract.php
+++ abstract.php
@@ -2,7 +2,7 @@
 	namespace sv_core;
 	
 	abstract class sv_abstract {
-		const version_core					= 4026;
+		const version_core					= 4030;
 		
 		protected $name						= false;
 		protected $module_name				= false;
