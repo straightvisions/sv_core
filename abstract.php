f6cc9448b9a77a46f8885b6760225b2cd97dfaf6
Dennis Heiden <dennis-heiden@straightvisions.com>
2020-08-26 14:59:27 +0200

    update vnum

--- abstract.php
+++ abstract.php
@@ -2,7 +2,7 @@
 	namespace sv_core;
 	
 	abstract class sv_abstract {
-		const version_core					= 4030;
+		const version_core					= 4031;
 		
 		protected $name						= false;
 		protected $module_name				= false;
