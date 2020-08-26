d1bf86e03f07481bcf505a2990e2d6c2097e2d13
Dennis Heiden <dennis-heiden@straightvisions.com>
2020-08-26 15:06:30 +0200

    add settings field password

---
+++ settings/tpl/setting_password.php
@@ -0,0 +1,14 @@
+<label for="<?php echo $props['ID']; ?>">
+	<input
+		data-sv_type="sv_form_field"
+		class="sv_input"
+		id="<?php echo $props['ID']; ?>"
+		name="<?php echo $props['name']; ?>"
+		type="password"
+		placeholder="<?php echo $props['placeholder']; ?>"
+		value="<?php echo esc_attr($props['value']); ?>"
+<?php echo ($props['maxlength'] ? 'maxlength="'.$props['maxlength'].'"' :  ''). '
+	' . $props['minlength'] . '
+	' . $props['required'] . '
+	' . $props['disabled']; ?>/>
+</label>
