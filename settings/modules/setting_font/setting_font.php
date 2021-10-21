50b8868a6922a3fef41a9f9f1b4357ae1f49119f
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-10-21 22:24:20 +0200

    FIX FONT SETTING

--- settings/modules/setting_font/setting_font.php
+++ settings/modules/setting_font/setting_font.php
@@ -2,24 +2,22 @@
 
 namespace sv_core;
 
-class setting_font extends settings {
-	private $parent	= false;
+class setting_font extends settings{
+	private $parent = false;
 
-	public function __construct( $parent = false ){
+	public function __construct($parent = false){
 		$this->parent = $parent;
-
-		add_action( 'after_setup_theme', array( $this, 'after_setup_theme' ) );
 	}
 
-	public function after_setup_theme() {
-		/* Enables the SV Color Picker for the color setting */
-		$color_input_id = $this->get_parent()->get_field_id() . '_color';
-		$color_input_value = '#000000';
+	public function get_css_data(string $custom_property = 'font-family', string $prefix = '', string $suffix = ''): array{
+		$properties = array();
 
-		if ( $this->get_parent()->get_data() && isset( $this->get_parent()->get_data()['color'] ) ) {
-			$color_input_value = $this->get_parent()->get_data()['color'];
+		if ($this->get_parent()->get_data()) {
+			foreach ($this->get_parent()->get_data() as $breakpoint => $val) {
+				$properties[$custom_property][$breakpoint] = $prefix.'"' . $val . '"'.$suffix;
+			}
 		}
 
-		$this->setting_color->localize_script( $color_input_id, $color_input_value );
+		return $properties;
 	}
 }
