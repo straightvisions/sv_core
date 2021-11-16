8c88700104e945713156cd6c32227724c236bdab
Matthias Bathke <matthias-bathke@straightvisions.com>
2021-11-16 16:10:16 +0100

    FIX COLOR PICKER AS GROUP FIELD

--- settings/tpl/setting_color.php
+++ settings/tpl/setting_color.php
@@ -2,18 +2,18 @@
 <script>
 	jQuery( document ).ready( function() {
 		if ( typeof SVColorPicker !== 'undefined' ) {
-			SVColorPicker.renderColorPicker( '<?php echo $props['name']; ?>', '<?php echo $color; ?>', <?php echo json_encode( get_theme_support( 'editor-color-palette' )[0] ) ?> );
+			SVColorPicker.renderColorPicker( '<?php echo $props['ID']; ?>', '<?php echo $color; ?>', <?php echo json_encode( get_theme_support( 'editor-color-palette' )[0] ) ?> );
 		}
 	} );
 </script>
 <div class="sv_setting_color_display" title="<?php echo __( 'Toggle Color Picker', 'sv_core' ); ?>">
 	<div class="sv_setting_color_value" style="background-color:rgba(<?php echo $color; ?>)"></div>
 </div>
-<label for="<?php echo $props['name']; ?>" class="sv_input_label_color sv_hidden">
+<label for="<?php echo $props['ID']; ?>" class="sv_input_label_color sv_hidden">
 	<input
 		data-sv_type="sv_form_field"
 		class="sv_input"
-		id="<?php echo $props['name']; ?>"
+		id="<?php echo $props['ID']; ?>"
 		name="<?php echo $props['name']; ?>"
 		type="color"
 		value="<?php echo $color; ?>"
