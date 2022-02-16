0cc0ba918fbae2c0d60546b5124f0eb0f42903d8
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-02-16 15:53:57 +0100

    MOVE ARCHIVE TEMPLATE DEFAULT SETTINGS FROM THEME MODULE TEMPLATE TO ABSTRACT UPDATE LANG

--- abstract_template_sv_archive.php
+++ abstract_template_sv_archive.php
@@ -2,7 +2,7 @@
 	namespace sv_core;
 
 	// this class could be part of different products
-	if(!class_exists('abstract_template_sv_archive')) {
+	if(!class_exists('sv_core')) {
 		// complete template logic should be here
 		class abstract_template_sv_archive{
 			protected $path				= '';
@@ -22,71 +22,71 @@
 				$this->parts		= array(
 					'common'				=> array(
 						'loaded'			=> true,
-						'label'				=> __('Common', 'template_sv_archive_list')
+						'label'				=> __('Common', 'sv_core')
 					),
 					'entry'					=> array(
 						'loaded'			=> true,
-						'label'				=> __('Entry', 'template_sv_archive_list')
+						'label'				=> __('Entry', 'sv_core')
 					),
 					'empty'					=> array(
 						'loaded'			=> false,
-						'label'				=> __('Empty', 'template_sv_archive_list')
+						'label'				=> __('Empty', 'sv_core')
 					),
 					'header'				=> array(
 						'loaded'			=> false,
-						'label'				=> __('Header', 'template_sv_archive_list')
+						'label'				=> __('Header', 'sv_core')
 					),
 					'footer'				=> array(
 						'loaded'			=> false,
-						'label'				=> __('Footer', 'template_sv_archive_list')
+						'label'				=> __('Footer', 'sv_core')
 					),
 					'sidebar_top'			=> array(
 						'loaded'			=> false,
-						'label'				=> __('Sidebar Top', 'template_sv_archive_list')
+						'label'				=> __('Sidebar Top', 'sv_core')
 					),
 					'sidebar_right'			=> array(
 						'loaded'			=> false,
-						'label'				=> __('Sidebar Right', 'template_sv_archive_list')
+						'label'				=> __('Sidebar Right', 'sv_core')
 					),
 					'sidebar_bottom'		=> array(
 						'loaded'			=> false,
-						'label'				=> __('Sidebar Bottom', 'template_sv_archive_list')
+						'label'				=> __('Sidebar Bottom', 'sv_core')
 					),
 					'sidebar_left'			=> array(
 						'loaded'			=> false,
-						'label'				=> __('Sidebar Left', 'template_sv_archive_list')
+						'label'				=> __('Sidebar Left', 'sv_core')
 					),
 					'featured_image'		=> array(
 						'loaded'			=> false,
-						'label'				=> __('Featured Image', 'template_sv_archive_list')
+						'label'				=> __('Featured Image', 'sv_core')
 					),
 					'title'					=> array(
 						'loaded'			=> false,
-						'label'				=> __('Title', 'template_sv_archive_list')
+						'label'				=> __('Title', 'sv_core')
 					),
 					'excerpt'				=> array(
 						'loaded'			=> false,
-						'label'				=> __('Excerpt', 'template_sv_archive_list')
+						'label'				=> __('Excerpt', 'sv_core')
 					),
 					'read_more'				=> array(
 						'loaded'			=> false,
-						'label'				=> __('Read More', 'template_sv_archive_list')
+						'label'				=> __('Read More', 'sv_core')
 					),
 					'author'				=> array(
 						'loaded'			=> false,
-						'label'				=> __('Author', 'template_sv_archive_list')
+						'label'				=> __('Author', 'sv_core')
 					),
 					'date'					=> array(
 						'loaded'			=> false,
-						'label'				=> __('Date', 'template_sv_archive_list')
+						'label'				=> __('Date', 'sv_core')
 					),
 					'date_modified'			=> array(
 						'loaded'			=> false,
-						'label'				=> __('Date Modified', 'template_sv_archive_list')
+						'label'				=> __('Date Modified', 'sv_core')
 					),
 					'categories'			=> array(
 						'loaded'			=> false,
-						'label'				=> __('Categories', 'template_sv_archive_list')
+						'label'				=> __('Categories', 'sv_core')
 					)
 				);
 
@@ -316,6 +316,345 @@
 							->run_type()
 							->add_child($setting);
 					}
+				}
+
+				return $this;
+			}
+			protected function load_settings_common(): abstract_template_sv_archive{
+				$this->get_setting( 'header_min_height', __('Common', 'sv_core') )
+					->set_title( __( 'Min Height', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Minimum Height Header', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->set_default_value('60vh')
+					->load_type( 'text' );
+
+				$this->get_setting( 'stack_active', __('Common', 'sv_core') )
+					->set_title( __( 'Stack Sidebar Columns', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'You may want to stack Sidebars on narrow viewports.', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->set_default_value(array(
+						'mobile'						=> 1,
+						'mobile_landscape'				=> 1,
+						'tablet'						=> 1,
+						'tablet_landscape'				=> 0,
+						'tablet_pro'					=> 0,
+						'tablet_pro_landscape'			=> 0,
+						'desktop'						=> 0
+					))
+					->load_type( 'checkbox' );
+
+				$this->get_setting('columns', __('Common', 'sv_core'))
+					->set_title( __( 'Columns', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Set number of columns for entries', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->set_default_value(array(
+						'mobile'						=> 1,
+						'mobile_landscape'				=> 2,
+						'tablet'						=> 2,
+						'tablet_landscape'				=> 2,
+						'tablet_pro'					=> 2,
+						'tablet_pro_landscape'			=> 2,
+						'desktop'						=> 2
+					))
+					->load_type( 'number' );
+
+				$this->get_setting( 'font', __('Common', 'sv_core') )
+					->set_title( __( 'Font Family', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Choose a font for your text.', 'abstract_template_sv_archive' ) )
+					->set_options( $this->get_instance()->get_module( 'sv_webfontloader' ) ? $this->get_instance()->get_module( 'sv_webfontloader' )->get_font_options() : array('' => __('Please activate module SV Webfontloader for this Feature.', 'sv_core')) )
+					->set_is_responsive(true)
+					->load_type( 'select' );
+
+				$this->get_setting( 'font_size', __('Common', 'sv_core') )
+					->set_title( __( 'Font Size', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Font Size in Pixel', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'number' );
+
+				$this->get_setting( 'line_height', __('Common', 'sv_core') )
+					->set_title( __( 'Line Height', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Set line height as multiplier or with a unit.', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'text' );
+
+				$this->get_setting( 'text_color', __('Common', 'sv_core') )
+					->set_title( __( 'Text Color', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'color' );
+
+				$this->get_setting( 'bg_color', __('Common', 'sv_core') )
+					->set_title( __( 'Background Color', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'color' );
+
+				$this->get_setting( 'column_spacing', __('Common', 'sv_core') )
+					->set_title( __( 'Column Spacing', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Set a Spacing in Pixel for columns', 'abstract_template_sv_archive' ) )
+					->set_default_value(10)
+					->load_type( 'text' );
+
+				$this->get_setting( 'margin', __('Common', 'sv_core') )
+					->set_title( __( 'Margin', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->set_default_value(
+						array(
+							'top'		=> '0',
+							'right'		=> 'auto',
+							'bottom'	=> '0',
+							'left'		=> 'auto'
+						)
+					)
+					->load_type( 'margin' );
+
+				$this->get_setting( 'padding', __('Common', 'sv_core') )
+					->set_title( __( 'Padding', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->set_default_value(
+						array(
+							'top'		=> '0',
+							'right'		=> '22px',
+							'bottom'	=> '0',
+							'left'		=> '22px'
+						)
+					)
+					->load_type( 'margin' );
+
+				$this->get_setting( 'border', __('Common', 'sv_core') )
+					->set_title( __( 'Border', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'border' );
+
+				return $this;
+			}
+			protected function load_settings_entry(): abstract_template_sv_archive{
+				$this->get_setting( 'entry_bg_color', __('Entry', 'sv_core') )
+					->set_title( __( 'Background Color', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'color' );
+
+				$this->get_setting( 'entry_margin', __('Entry', 'sv_core') )
+					->set_title( __( 'Margin', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->set_default_value(
+						array(
+							'top'		=> '0',
+							'right'		=> '0',
+							'bottom'	=> '40px',
+							'left'		=> '0'
+						)
+					)
+					->load_type( 'margin' );
+
+				$this->get_setting( 'entry_padding', __('Entry', 'sv_core') )
+					->set_title( __( 'Padding', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'margin' );
+
+				$this->get_setting( 'entry_border', __('Entry', 'sv_core') )
+					->set_title( __( 'Border', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'border' );
+
+				return $this;
+			}
+			protected function load_settings_parts(): abstract_template_sv_archive{
+				$this->get_setting('show_header', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Archive Header', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_footer', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Archive Footer', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_empty', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Notice when empty', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_featured_image', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Featured Image', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_title', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Title', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_excerpt', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Excerpt', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_read_more', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Read More', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_author', __('Parts', 'template_sv_archive_pba_default'))
+					->set_title( __( 'Show Author', 'template_sv_archive_pba_default' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'template_sv_archive_pba_default' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_date', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Date', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_date_modified', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Date Modified', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(0)
+					->load_type( 'checkbox' );
+
+				$this->get_setting('show_categories', __('Parts', 'sv_core'))
+					->set_title( __( 'Show Categories', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Show or Hide this Template Part', 'abstract_template_sv_archive' ) )
+					->set_default_value(1)
+					->load_type( 'checkbox' );
+
+				return $this;
+			}
+			protected function load_settings_part(string $part): abstract_template_sv_archive{
+				$this->get_setting( $part.'_order', $part )
+					->set_title( __( 'Order', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Order part', 'abstract_template_sv_archive' ) )
+					->set_options(
+						array('' => __('Default', 'sv_core'))
+						+array_combine(
+							range(1, count($this->get_parts())),
+							range(1, count($this->get_parts()))
+						)
+					)
+					->set_is_responsive(true)
+					->load_type( 'select' );
+
+				$this->get_setting( $part.'_font', $part )
+					->set_title( __( 'Font Family', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Choose a font for your text.', 'abstract_template_sv_archive' ) )
+					->set_options( $this->get_instance()->get_module( 'sv_webfontloader' ) ? $this->get_instance()->get_module( 'sv_webfontloader' )->get_font_options() : array('' => __('Please activate module SV Webfontloader for this Feature.', 'sv_core')) )
+					->set_is_responsive(true)
+					->load_type( 'select' );
+
+				$this->get_setting( $part.'_font_size', $part )
+					->set_title( __( 'Font Size', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Font Size in Pixel', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'number' );
+
+				$this->get_setting( $part.'_line_height', $part )
+					->set_title( __( 'Line Height', 'abstract_template_sv_archive' ) )
+					->set_description( __( 'Set line height as multiplier or with a unit.', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'text' );
+
+				$this->get_setting( $part.'_text_color', $part )
+					->set_title( __( 'Text Color', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'color' );
+
+				$this->get_setting( $part.'_bg_color', $part )
+					->set_title( __( 'Background Color', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'color' );
+
+				$this->get_setting( $part.'_text_color_hover', $part )
+					->set_title( __( 'Text Color Hover', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'color' );
+
+				$this->get_setting( $part.'_bg_color_hover', $part )
+					->set_title( __( 'Background Color Hover', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'color' );
+
+				$this->get_setting( $part.'_margin', $part )
+					->set_title( __( 'Margin', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'margin' );
+
+				$this->get_setting( $part.'_padding', $part )
+					->set_title( __( 'Padding', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'margin' );
+
+				$this->get_setting( $part.'_border', $part )
+					->set_title( __( 'Border', 'abstract_template_sv_archive' ) )
+					->set_is_responsive(true)
+					->load_type( 'border' );
+
+				$this->load_settings_part_default_values($part);
+
+				return $this;
+			}
+			protected function load_settings_part_default_values(string $part): abstract_template_sv_archive{
+				if(
+					$part == 'featured_image'
+					|| $part == 'title'
+					|| $part == 'excerpt'
+				){
+					$this->get_setting( $part.'_margin', $part )->set_default_value(
+						array(
+							'top'		=> '0',
+							'right'		=> '0',
+							'bottom'	=> '20px',
+							'left'		=> '0'
+						)
+					);
+				}
+
+				if($part == 'title'){
+					$this->get_setting( $part.'_font_size', $part )->set_default_value(20);
+				}
+
+				if(
+					$part == 'read_more'
+				){
+					$this->get_setting( $part.'_padding', $part )->set_default_value(
+						array(
+							'top'		=> '0',
+							'right'		=> '10px',
+							'bottom'	=> '0',
+							'left'		=> '0'
+						)
+					);
+				}
+
+				if(
+					$part == 'date'
+				){
+					$this->get_setting( $part.'_padding', $part )->set_default_value(
+						array(
+							'top'		=> '0',
+							'right'		=> '10px',
+							'bottom'	=> '0',
+							'left'		=> '10px'
+						)
+					);
+				}
+
+				if(
+					$part == 'categories'
+				){
+					$this->get_setting( $part.'_padding', $part )->set_default_value(
+						array(
+							'top'		=> '0',
+							'right'		=> '0',
+							'bottom'	=> '0',
+							'left'		=> '10px'
+						)
+					);
 				}
 
 				return $this;
