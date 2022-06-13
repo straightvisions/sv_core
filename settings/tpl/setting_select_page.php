31debc9a6bf450050a5b818fc13d59ef59e9444f
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-13 18:21:41 +0200

    fix setting: select page  - show private pages, too

--- settings/tpl/setting_select_page.php
+++ settings/tpl/setting_select_page.php
@@ -1,12 +1,13 @@
 <label for="<?php echo $props['ID']; ?>">
 <?php
 	$args		= array(
-	'echo'					=> 0,
-	'selected'				=> $props['value'],
-	'name'					=> $props['name'],
-	'id'					=> $props['ID'],
-	'class'					=> 'data_sv_type_sv_form_field sv_input',
-	'show_option_none'		=> __('No Page selected', 'sv_core')
+		'echo'					=> 0,
+		'selected'				=> $props['value'],
+		'name'					=> $props['name'],
+		'id'					=> $props['ID'],
+		'class'					=> 'data_sv_type_sv_form_field sv_input',
+		'show_option_none'		=> __('No Page selected', 'sv_core'),
+		'post_status'			=> 'publish,private',
 	);
 	echo wp_dropdown_pages($args);
 ?>
